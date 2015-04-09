580fafcf7f37a537189ffaa0a3013f4b207caca8
hcthartmann <hcthartmann_github@hh2013.33mail.com>
2015-04-10 00:14:44 +0200

    =btcchina

---
+++ yats/src/main/java/org/yats/connectivity/xchange/BtcchinaPricePoll.java
@@ -0,0 +1,10 @@
+package org.yats.connectivity.xchange;
+
+/**
+ * Created
+ * Date: 09/04/15
+ * Time: 22:46
+ */
+public class BtcchinaPricePoll
+{
+}
---
+++ yats/src/main/java/org/yats/trader/examples/server/BtcchinaPricefeedMain.java
@@ -0,0 +1,10 @@
+package org.yats.trader.examples.server;
+
+/**
+ * Created
+ * Date: 09/04/15
+ * Time: 22:44
+ */
+public class BtcchinaPricefeedMain
+{
+}
